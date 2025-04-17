d8cf99cc62ba05a39255e2e2e72eb2519ebf5ddf
Pieter Prinsloo <pietersp@gmail.com>
2025-04-17 09:08:14 +0200

    Add department opertations to client

--- client/src/main/scala/com/example/client/ExampleClient.scala
+++ client/src/main/scala/com/example/client/ExampleClient.scala
@@ -3,7 +3,7 @@ package com.example.client
 import zio.*
 import com.example.api.endpoint.DepartmentEndpoints
 import com.example.domain.{Department, DepartmentId}
-import com.example.error.AppError.DepartmentAlreadyExists
+import com.example.error.AppError.{DepartmentAlreadyExists, DepartmentNotFound}
 import zio.http.{Client, URL}
 import zio.http.endpoint.{EndpointExecutor, EndpointLocator}
 
@@ -19,16 +19,30 @@ final case class ExampleClient(
 
   def createDepartment(
     department: Department
-  ): IO[DepartmentAlreadyExists, DepartmentId] = {
-    ZIO.scoped {
-      executor(departmentEndpoints.createDepartment(department))
-    }
+  ): IO[DepartmentAlreadyExists, DepartmentId] =
+    ZIO.scoped(executor(departmentEndpoints.createDepartment(department)))
 
-  }
+  def getDepartments: UIO[Vector[Department]] =
+    ZIO.scoped(executor(departmentEndpoints.getDepartments(())))
+
+  def getDepartmentById(id: DepartmentId): IO[DepartmentNotFound, Department] =
+    ZIO.scoped(executor(departmentEndpoints.getDepartmentById(id)))
+
+  def updateDepartment(
+    id: DepartmentId,
+    department: Department
+  ): IO[DepartmentNotFound, Unit] =
+    ZIO.scoped(executor(departmentEndpoints.updateDepartment(id, department)))
+
+  def deleteDepartment(id: DepartmentId): UIO[Unit] =
+    ZIO.scoped(executor(departmentEndpoints.deleteDepartment(id)))
+
+
 }
 
 object ExampleClient {
   case class MyConfig(url: String, timeout: Int, retries: Int)
+
   object MyConfig {
 
     import zio.Config.*
@@ -37,7 +51,7 @@ object ExampleClient {
       (string("url") ++ int("timeout") ++ int("retries"))
         .map(MyConfig(_, _, _))
 
-    val defaultLayer =
+    val defaultLayer: ULayer[IO[Error, MyConfig]] =
       ZLayer.succeed(
         ConfigProvider
           .fromMap(
